456166b21b9e0abd0be01bc5ea770ed4c2a0d17d
Thuynguyen2004 <nguyenthithuytb411@gmail.com>
2025-10-27 18:21:19 +0700

    Day vi du code

--- bai8/script.js
+++ bai8/script.js
@@ -1,1 +1,56 @@
 document.writeln('Xin chào');
+//<!DOCTYPE html>
+//<html lang="vi">
+//<head>
+  //<meta charset="UTF-8">
+  //<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //<title>Tính chỉ số BMI</title>
+//<head>//
+//<body>
+  //<h2>Tính chỉ số BMI</h2>
+
+  //Cân nặng (kg): <input type="number" id="weight"><br>
+  //Chiều cao (m): <input type="number" id="height" step="0.01"><br>
+  //<button onclick="tinhBMI()">Kiểm tra BMI</button>
+
+ // <p id="result"></p>
+  //<img id="image" width="150" style="display:none">
+
+ // <script>
+    //function tinhBMI() {
+      //var canNang = parseFloat(document.getElementById("weight").value);
+      //var chieuCao = parseFloat(document.getElementById("height").value);
+    //  var ketQua = document.getElementById("result");
+    //  var hinhAnh = document.getElementById("image");
+
+     // if (isNaN(canNang) || isNaN(chieuCao) || canNang <= 0 || chieuCao <= 0) {
+        //ketQua.innerHTML = "⚠️ Vui lòng nhập đúng cân nặng và chiều cao!";
+     //   hinhAnh.style.display = "none";
+     //   return;
+     // }
+
+      //var bmi = canNang / (chieuCao * chieuCao);
+     // var loai = "";
+      //var linkAnh = "";
+
+      //if (bmi < 18.5) {
+       // loai = "Gầy";
+     //   linkAnh = "https://cdn-icons-png.flaticon.com/512/4472/4472519.png";
+     // } else if (bmi < 24.9) {
+       // loai = "Bình thường";
+       // linkAnh = "https://cdn-icons-png.flaticon.com/512/2920/2920244.png";
+     // } else if (bmi < 29.9) {
+       // loai = "Thừa cân";
+      //  linkAnh = "https://cdn-icons-png.flaticon.com/512/4151/4151022.png";
+      //} else {
+        //loai = "Béo phì";
+       // linkAnh = "https://cdn-icons-png.flaticon.com/512/3048/3048127.png";
+      //}
+
+     // ketQua.innerHTML = `Chỉ số BMI: <b>${bmi.toFixed(2)}</b> (${loai})`;
+     // hinhAnh.src = linkAnh;
+      ////hinhAnh.style.display = "block";
+  //  }
+ // </script>
+//</body>
+//</html>
